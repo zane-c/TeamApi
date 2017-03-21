166cc0d0f8f422af5ce6286256d8c8ee2cd9c804
Zane Christenson <zane.c@berkeley.edu>
2017-03-20 22:46:26 -0700

    removed brains

--- TeamMemberApi/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ListState from './ListState'
-import './App.css'
-
-ReactDOM.render(
-  <ListState />,
-  document.getElementById('container')
-)
