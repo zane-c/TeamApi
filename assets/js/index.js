6eb6ff15630b516ef253416754f5b5b4fcc2c126
Zane Christenson <zane.c@berkeley.edu>
2017-03-21 19:59:56 -0700

    added semicolons and const

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import List from './List'
-import './App.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+import './App.css';
 
 /*
     Launches the React UI by creating a List view
     and places it in index.html
 */
-ReactDOM.render(
-  <List />,
-  document.getElementById('container')
-)
+ReactDOM.render(<List />, document.getElementById('container'));
